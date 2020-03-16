7203f8094648f9f27d7424c605eea4ab133ab34b
bohdansemyvolos <b.semivolos@agilites.com>
2020-03-16 18:33:30 +0200

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let arr = []
+    for (let s of str) {
+        let isBracketCorrect = false;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (s === bracketsConfig[i][1] && arr[arr.length - 1] === bracketsConfig[i][0]) {
+                arr.pop();
+                isBracketCorrect = true;
+            } else if (s === bracketsConfig[i][0]) {
+                arr.push(s);
+                isBracketCorrect = true;
+            }
+        }
+        if (!isBracketCorrect) {
+            return false
+        }
+    }
+    return arr.length === 0;
 }
